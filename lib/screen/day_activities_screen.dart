ac370cc551c900fd92b4084a981a089eaf2761a6
Krzysztof Aniołkowski <kjaniol94@gmail.com>
2023-05-14 19:39:56 +0200

    List content added to a day

--- lib/screen/day_activities_screen.dart
+++ lib/screen/day_activities_screen.dart
@@ -3,27 +3,21 @@ import '../model/activity.dart';
 
 //View of activities in a day
 class DayActivitiesScreen extends StatelessWidget {
-  const DayActivitiesScreen(
-      {super.key, required this.title, required this.activities});
+  const DayActivitiesScreen({super.key, required this.activities});
 
-  final String title;
   final List<Activity> activities;
 
   @override
   Widget build(BuildContext context) {
+    //List view
     Widget listContent = ListView.builder(
         itemCount: activities.length,
         itemBuilder: (context, index) => Text(activities[index].title));
 
-    if (activities.isNotEmpty) {
-      listContent;
-    } else {
-      Text('No Activities');
+    if (activities.isEmpty) {
+      listContent = const Text('No Activities!');
     }
 
-    return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      body: listContent,
-    );
+    return Scaffold(appBar: AppBar(title: Text('Hej')), body: listContent);
   }
 }
