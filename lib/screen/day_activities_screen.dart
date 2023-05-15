4f5c1d9d923b05eb2638d6f523e83b5d37c91b32
Krzysztof Aniołkowski <kjaniol94@gmail.com>
2023-05-15 09:12:15 +0200

    BuildListContent method added

--- lib/screen/day_activities_screen.dart
+++ lib/screen/day_activities_screen.dart
@@ -1,23 +1,31 @@
 import 'package:flutter/material.dart';
 import '../model/activity.dart';
 
-//View of activities in a day
+//Screen of activities in a day
 class DayActivitiesScreen extends StatelessWidget {
   const DayActivitiesScreen({super.key, required this.activities});
 
   final List<Activity> activities;
 
-  @override
-  Widget build(BuildContext context) {
-    //List view
+  //Widget builds list of activities if not empty
+  Widget buildListContent(BuildContext context) {
     Widget listContent = ListView.builder(
         itemCount: activities.length,
         itemBuilder: (context, index) => Text(activities[index].title));
 
     if (activities.isEmpty) {
-      listContent = const Text('No Activities!');
+      listContent = Center(
+          child: const Text(
+        'No Activities today yet!',
+        style: TextStyle(fontSize: 30),
+      ));
     }
+    return listContent;
+  }
 
-    return Scaffold(appBar: AppBar(title: Text('Hej')), body: listContent);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Hej')), body: buildListContent(context));
   }
 }
