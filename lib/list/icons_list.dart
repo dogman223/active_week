b601b3000b31af5b6268b0c6c9570f3310dc0f4d
dogman223 <kjaniol94@gmail.com>
2023-07-14 15:36:04 +0200

    Add functionality that displays icon for each element of list depending on category

---
+++ lib/list/icons_list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+var iconsList = [
+  const Icon(Icons.sports_basketball),
+  const Icon(Icons.fastfood),
+  const Icon(Icons.play_arrow),
+  const Icon(Icons.book),
+  const Icon(Icons.airplanemode_active),
+  const Icon(Icons.bed),
+  const Icon(Icons.people),
+  const Icon(Icons.church),
+  const Icon(Icons.star),
+  const Icon(Icons.child_friendly),
+  const Icon(Icons.handshake),
+  const Icon(Icons.work),
+  const Icon(Icons.health_and_safety),
+];
--- lib/widgets/activity_item.dart
+++ lib/widgets/activity_item.dart
@@ -1,33 +1,72 @@
 import 'package:flutter/material.dart';
 
 import 'package:active_week/model/activity.dart';
+import 'package:active_week/list/icons_list.dart';
 
 //Appearance of activity element in list
-class ActivityItem extends StatelessWidget {
-  const ActivityItem(
-      {super.key, required this.activity, required this.deleteActivity});
+class ActivityItem extends StatefulWidget {
+  const ActivityItem({
+    super.key,
+    required this.activity,
+    required this.deleteActivity,
+  });
 
   final Activity activity;
   final Function deleteActivity;
 
   @override
+  State<ActivityItem> createState() => _ActivityItemState();
+}
+
+class _ActivityItemState extends State<ActivityItem> {
+  //Method adds icon as leading element to each element of the list.
+  addIcon() {
+    Icon icon;
+    switch (widget.activity.category.name) {
+      case 'sport':
+        return icon = iconsList[0];
+      case 'meal':
+        return icon = iconsList[1];
+      case 'entertainment':
+        return icon = iconsList[2];
+      case 'learning':
+        return icon = iconsList[3];
+      case 'travel':
+        return icon = iconsList[4];
+      case 'rest':
+        return icon = iconsList[5];
+      case 'social':
+        return icon = iconsList[6];
+      case 'spirituality':
+        return icon = iconsList[7];
+      case 'hobby':
+        return icon = iconsList[8];
+      case 'family':
+        return icon = iconsList[9];
+      case 'friends':
+        return icon = iconsList[10];
+      case 'work':
+        return icon = iconsList[11];
+      case 'health':
+        return icon = iconsList[12];
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return ListTile(
-        leading: const Icon(
-          Icons.tag,
-          color: Colors.green,
-        ),
+        leading: addIcon(),
         title: Card(
           elevation: 5,
           child: Text(
-            activity.title,
+            widget.activity.title,
             style: const TextStyle(fontWeight: FontWeight.bold),
           ),
         ),
-        subtitle: Text(activity.category.name),
+        subtitle: Text(widget.activity.category.name),
         trailing: IconButton(
           icon: const Icon(Icons.delete),
-          onPressed: () => deleteActivity(activity),
+          onPressed: () => widget.deleteActivity(widget.activity),
         ));
   }
 }
